6cdf54173dbc3e39c23335e22d8d510020973a9a
Bruno Lima <bg.lima@hotmail.com>
2016-11-01 23:25:39 -0200

    Sending commands at RAM via serial. TODO: 1: Check if busy before send. 2: Implement receive routine

--- main.asm
+++ main.asm
@@ -20,15 +20,21 @@
 ; Definitions for LCD controls
 dataPort EQU	PORTC
 ctrlPort EQU	PORTA
-i	 EQU	0x20		; Index for loop
-j	 EQU	0x21		; Index for loop
-hi	 EQU	0x22		; Most  significant nibble from data/command
-lo	 EQU	0x23		; Least significant nibble from data/command
+i	 	 EQU	0x20		; Index for loop
+j	 	 EQU	0x21		; Index for loop
+hi	 	 EQU	0x22		; Most  significant nibble from data/command
+lo	 	 EQU	0x23		; Least significant nibble from data/command
+
+; Define command lengths = (num of chars) + 3 (0x0D 0x0A 0x00)
+ATCOM	 UDATA	0x24
+ATCom01	 RES	D'9'		; command "AT+RST"
+ATCom02	 RES	D'14'		; command "AT+CIPMUX=1"
+ATCom03	 RES	D'34'		; command "AT+CWJAP="IC","icomputacaoufal""
 
 #define	rs	ctrlPort, 0	; R/S (Data/Command)
 #define	en	ctrlPort, 1	; Enable
-#define	btn1 ctrlPort, 4 ; Button 1
-#define btn2 ctrlPort, 5 ; Button 2
+#define	btn1 	ctrlPort, 4 ; Button 1
+#define btn2 	ctrlPort, 5 ; Button 2
 
 
 ;lcdPrt	 UDATA	0x05	
@@ -53,12 +59,15 @@ lo	 EQU	0x23		; Least significant nibble from data/command
 ;====================================================================
 #include "C:\Users\Bruno\git\Attendant\lcdModule\lcdFunctions.inc"
 #include "C:\Users\Bruno\git\Attendant\btnModule\btnFunctions.inc"
+#include "C:\Users\Bruno\git\Attendant\serialModule\serialModule.inc"
+#include "C:\Users\Bruno\git\Attendant\serialModule\commandModule.inc"
 
 setup:  
       ; PIC pre configurations
       BANKSEL	CMCON0	 ; Bank 1 selction
       MOVLW	0x07
       MOVWF	CMCON0	 ; Disabling comparators
+      
       BANKSEL	ANSEL
       CLRF	ANSEL	 ; Disabling analog I/O
       MOVLW	B'111000'; RA[0..2]=output, RA[3..5]=input
@@ -80,14 +89,29 @@ setup:
       MOVWF	IOCA		; Interrupt on change active on RA5 and RA4
       BSF	INTCON, RAIE; PORTA change interrupt Enable bit
       BCF	INTCON, RAIF; Clearing RAIF Flag
+      MOVLW   	0x70
+      MOVWF   	OSCCON  ; set for 8-MHz internal clock 
       BANKSEL 	ctrlPort
+      
+      ; Configuring USART
+      
+      MOVLW   d'12'           ;9600 baud
+      MOVWF   SPBRG
+      BCF     PIR1,  RCIF     ;clear RX interrupt
+      BSF     RCSTA, SPEN     ;serial port enable
+      BSF     RCSTA, CREN     ;continuous RX enable
+      BCF     TXSTA, SYNC     ;asynchronous mode
+      BSF     TXSTA, TXEN     ;enable transmitter
+      
+      CALL     loadCommands
       GOTO	loop
          
 ;====================================================================
 ; MAIN LOOP
 ;==================================================================== 
       
-loop:
+loop:	
+      CALL	delay
       GOTO	loop
     
 ;====================================================================
